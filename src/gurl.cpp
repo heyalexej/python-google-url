99cc22af6e84575a76cbd7f7667bd1ba59eeecd9
flier.lu <flier.lu@74e8857a-234a-11de-96a1-5542d1fc8aa6>
2009-04-07 09:44:48 +0000

    add unit test scripts

--- src/gurl.cpp
+++ src/gurl.cpp
@@ -1,6 +1,23 @@
 #include "gurl.h"
 
+std::ostream& operator <<(std::ostream& os, const CUrlWrapper& wrapper)
+{ 
+  os << static_cast<const GURL&>(wrapper);
+
+  return os;
+}
+
 BOOST_PYTHON_MODULE(gurl)
 {
+  py::class_<CUrlWrapper>("Url", py::init<>())
+    .def(py::init<const std::string>())
+    .def(py::init<const std::wstring>())
+    .def(py::init<const CUrlWrapper>())
 
+    .def(str(py::self))
+
+    .def("__nonzero__", &CUrlWrapper::operator bool)
+
+    .def("empty", &CUrlWrapper::empty)
+    ;
 }
