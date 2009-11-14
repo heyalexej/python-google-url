a7f76abf768cb500975713039d9243c00d27eda2
flier.lu <flier.lu@74e8857a-234a-11de-96a1-5542d1fc8aa6>
2009-11-14 05:10:18 +0000

    add fake logging for Linux

---
+++ trunk/src/logging.cpp
@@ -0,0 +1,32 @@
+#include "base/logging.h"
+
+using namespace logging;
+
+LogMessage::LogMessage(const char* file, int line, LogSeverity severity, int ctr)
+{
+
+}
+
+LogMessage::LogMessage(const char* file, int line)
+{
+
+}
+
+LogMessage::LogMessage(const char* file, int line, LogSeverity severity)
+{
+
+}
+
+LogMessage::LogMessage(const char* file, int line, const CheckOpString& result)
+{
+
+}
+
+LogMessage::~LogMessage()
+{
+
+}
+
+void LogMessage::Init(const char* file, int line)
+{
+}
